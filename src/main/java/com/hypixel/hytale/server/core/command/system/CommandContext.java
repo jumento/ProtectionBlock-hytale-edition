590cd24af9fd1609383c422d8b8c1074d7259dea
jumento <jumentolandia@gmail.com>
2026-01-13 15:02:27 -0600

    Remove unused imports in Command stubs

--- src/main/java/com/hypixel/hytale/server/core/command/system/CommandContext.java
+++ src/main/java/com/hypixel/hytale/server/core/command/system/CommandContext.java
@@ -1,7 +1,5 @@
 package com.hypixel.hytale.server.core.command.system;
 
-import com.hypixel.hytale.server.core.entity.entities.Player;
-
 public class CommandContext {
     public CommandSender sender() {
         return null;
--- src/main/java/com/hypixel/hytale/server/core/command/system/CommandSender.java
+++ src/main/java/com/hypixel/hytale/server/core/command/system/CommandSender.java
@@ -1,7 +1,5 @@
 package com.hypixel.hytale.server.core.command.system;
 
-import com.hypixel.hytale.server.core.entity.entities.Player;
-
 public interface CommandSender {
     void sendMessage(String message);
 }
